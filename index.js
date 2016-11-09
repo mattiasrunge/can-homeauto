c371ad80298ce98ad629a322ccf37574455e9c39
Linus Lundin <linus.lundin@gmail.com>
2016-11-09 20:42:08 +0000

    Moved Mqtt handling out to separate class, work remainging to get it up and running Added configuration files for all settings, remove .template from file name.

--- index.js
+++ index.js
@@ -3,40 +3,18 @@
 const path = require("path");
 const Connection = require("./lib/connection");
 
-const host = process.argv[2] || "192.168.1.250";
-const port = parseInt(process.argv[3], 10) || 1100;
-const protocol = process.argv[4] || path.join(__dirname, "protocol.xml");
-const alias = process.argv[5] || path.join(__dirname, "alias");
+const configuration = require("./configuration.json");
 
-var mqtt = require('mqtt');
-
-var url = "mqtt://hok.famlundin.org";
-
-var options = {
-  port: 1883,
-  clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
-  username: 'linus',
-  password: 'linusPass',
-};
-
-var mqttclient  = mqtt.connect(url, options);
-mqttclient.on('connect', function () {
-  //client.subscribe('presence')
-  mqttclient.publish('presence', 'Hello mqtt from nodejs')
-})
- 
-mqttclient.on('message', function (topic, message) {
-  // message is Buffer 
-  console.log(message.toString())
-  mqttclient.end()
-})
-
+const host = process.argv[2] || configuration.can_network.ip;
+const port = parseInt(process.argv[3], 10) || configuration.can_network.port;
+const protocol = process.argv[4] || configuration.can_network.protocol;
+const alias = process.argv[5] || configuration.can_network.alias;
 
 console.log(`Protocol definition: ${protocol}`);
 console.log(`Alias definition: ${alias}`);
 console.log(`Connecting to ${host}:${port}`);
 
-const conn = new Connection(protocol, host, port, alias);
+const conn = new Connection(protocol, host, port, alias, configuration);
 
 conn.on("error", (error) => {
     console.error(error);
@@ -49,7 +27,7 @@ conn.on("message", (message) => {
 
 conn.on("mqttmessage", (message)  => {
     console.log("Message", JSON.stringify(message));
-    mqttclient.publish(message.topic, message.value.toString());
+    //mqttclient.publish(message.topic, message.value.toString());
 });
 
 conn.start()
