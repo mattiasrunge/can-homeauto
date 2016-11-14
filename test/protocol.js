6678848fa682171add6d144835625fe9d10624fe
Mattias Runge <mattias@runge.se>
2016-11-14 21:42:40 +0100

    Start stuff for tests

---
+++ test/protocol.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/* global describe it */
+
+const { assert } = require("chai");
+// const Protocol
+
+
+describe("Protocol", () => {
+    describe("", () => {
+        it("shall ...", async () => {
+        });
+    });
+});
